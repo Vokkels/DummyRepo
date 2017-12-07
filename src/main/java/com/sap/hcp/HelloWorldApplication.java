47dead8ef76be442a833ea7df4a10bd8cfb5515e
Daniel.Malan <13danielmalan@gmail.com>
2017-12-07 14:45:02 +0200

    Test Hello world

--- src/main/java/com/sap/hcp/HelloWorldApplication.java
+++ src/main/java/com/sap/hcp/HelloWorldApplication.java
@@ -17,7 +17,7 @@ public class HelloWorldApplication extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello WOrld 2");
+        resp.getWriter().println("Hello World Update 2");
     }
 
     @Override
